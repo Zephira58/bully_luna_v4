347df42d7eed8ef40322af541a3272690dee2beb
Xanthus <Xanthus58@protonmail.com>
2022-09-01 03:03:03 +0930

    Add env support

--- src/egui/api_handler.rs
+++ src/egui/api_handler.rs
@@ -1,4 +1,7 @@
 #![allow(clippy::needless_return)]
+#![allow(non_snake_case)]
+use dotenv;
+
 
 pub fn get_insult() -> String {
     //View more details on how this works here (https://www.reddit.com/r/learnrust/comments/wz9flc/how_to_get_request_to_return_as_a_string/)
@@ -10,9 +13,10 @@ pub fn get_insult() -> String {
 }
 
 use webhook::client::{WebhookClient, WebhookResult};
-const IMAGE_URL: &'static str = "https://cdn.discordapp.com/avatars/292971545956188160/eab559efa07f0f3dd13d21ac5f26c4ce.png?size=1024";
+
 pub async fn send_message(msg: &str) -> WebhookResult<()> {
-    let url = "https://canary.discord.com/api/webhooks/1014531600547074070/IAmOcsyW0QjAAsM6gKdVNrjgOxLoUZAjakURSjWjbuuC61GlJDL5cDxgobjP-kqTDosH";
+    let IMAGE_URL = dotenv::var("IMAGE_URL").unwrap();
+    let url = dotenv::var("WEBHOOK_URL").unwrap();
     let client = WebhookClient::new(&url);
     let webhook_info = client.get_information().await?;
     println!("webhook: {:?}", webhook_info);
@@ -22,7 +26,7 @@ pub async fn send_message(msg: &str) -> WebhookResult<()> {
             message
                 .content(msg)
                 .username("Xanthus")
-                .avatar_url(IMAGE_URL)
+                .avatar_url(&IMAGE_URL)
         })
         .await?;
 
