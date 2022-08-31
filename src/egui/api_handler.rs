92c735882b8f50937af4e83cba5f9ee4b31b1081
Xanthus <Xanthus58@protonmail.com>
2022-09-01 05:42:03 +0930

    add env

--- src/egui/api_handler.rs
+++ src/egui/api_handler.rs
@@ -16,8 +16,10 @@ pub fn get_insult() -> String {
 pub async fn send_message(msg: &str) {
     let WEBHOOK_URL = dotenv::var("WEBHOOK_URL").expect("WEBHOOK_URL not found");
     let IMAGE_URL = dotenv::var("IMAGE_URL").expect("IMAGE_URL not found");
+    let DISCORD_ID = dotenv::var("DISCORD_ID").expect("DISCORD_ID not found");
 
-    let message = msg.to_owned() + " <@747638440404713582>";
+
+    let message = msg.to_owned() + &DISCORD_ID;
     let message = message.as_str();
     let mut request_body = HashMap::new();
     request_body.insert("content", message);
